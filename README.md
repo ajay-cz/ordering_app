f6cbb82f5ad362b743807baa32e884be82dad4ac
Ajaykumar. R <ajay.calitz@gmail.com>
2015-12-15 18:21:07 +0530

    First Cut of Code changes
    1) Adding a Base Template
    2) Included BootStrap Material Theme
    3) A simplified form of Partial page loading ( TODO: History mgmt )

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ordering_app
 An Ordering App
+
+
+To add a link to a page:
+add a html page to partials and in the anchro tag, specify role="page-nav" href="/home"
+eg.,
+<a role="page-nav" href="/home" id="home">
---
+++ src/static/app/js/app.js
@@ -0,0 +1,34 @@
+/**
+ *
+ */
+
+$(document).ready(function () {
+    // Initialize Bootstrap Material Theme
+    $.material.init();
+
+    // Code to Try loading the Partial Page
+    $('a[role=page-nav]').off('click').on('click', function (event) {
+        var clicked_link = $(this), link = $(clicked_link).attr('href');
+        if (link) {
+            event.preventDefault();
+            $.ajax({
+                url: 'partials' + link + '.html',
+                type: 'GET'
+            }).done(function(data, textStatus, jqXHR){
+                $('#body-content').html($.parseHTML(data));
+                // Highlights the corresponding link
+                $('a[role=page-nav]').parents().removeClass('active');
+                $(clicked_link).parent().addClass('active');
+//                window.history.pushState(null, "", link);
+            }).fail(function( jqXHR, textStatus, errorThrown){
+                if (jqXHR && jqXHR.status === 404) {
+                    alert(link + '.html Not Found under partials directory ! Check https://github.com/ajay-cz/ordering_app/wiki/Rules-to-follow')
+                }
+            })
+        }
+        else {
+            alert('Kindly check the anchor tag . For a New Page Link, add <a role="page-nav" href="/<path_to_navigate>" ...></a>')
+        }
+    });
+    $('#home').trigger('click');
+});
